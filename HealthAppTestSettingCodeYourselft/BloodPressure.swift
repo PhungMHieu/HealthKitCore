78f3feb1be658e9223aa6d3ec73801700bfe2bb4
PhungMHieu <kiritosad41@gmail.com>
2025-08-21 17:38:33 +0700

    first commit

---
+++ HealthAppTestSettingCodeYourselft/BloodPressure.swift
@@ -0,0 +1,8 @@
+//
+//  BloodPressure.swift
+//  HealthAppTestSettingCodeYourselft
+//
+//  Created by iKame Elite Fresher 2025 on 21/8/25.
+//
+
+import Foundation
---
+++ HealthAppTestSettingCodeYourselft/HealthKitManager.swift
@@ -0,0 +1,8 @@
+//
+//  HealthKitManager.swift
+//  HealthAppTestSettingCodeYourselft
+//
+//  Created by iKame Elite Fresher 2025 on 21/8/25.
+//
+
+import Foundation
