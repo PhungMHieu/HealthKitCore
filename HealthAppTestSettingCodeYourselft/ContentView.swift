d182218d1db941c2869320c06a96f9296f6dbb5e
PhungMHieu <kiritosad41@gmail.com>
2025-08-21 09:37:09 +0700

    Initial Commit

---
+++ HealthAppTestSettingCodeYourselft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthAppTestSettingCodeYourselft
+//
+//  Created by iKame Elite Fresher 2025 on 21/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthAppTestSettingCodeYourselft/HealthAppTestSettingCodeYourselftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppTestSettingCodeYourselftApp.swift
+//  HealthAppTestSettingCodeYourselft
+//
+//  Created by iKame Elite Fresher 2025 on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppTestSettingCodeYourselftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
