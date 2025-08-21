bbfe13cb6334160117d91d5add59fa4910e242e7
PhungMHieu <kiritosad41@gmail.com>
2025-08-22 02:06:43 +0700

    update manager

--- HealthAppTestSettingCodeYourselft/ContentView.swift
+++ HealthAppTestSettingCodeYourselft/ContentView.swift
@@ -7,26 +7,74 @@
 
 import SwiftUI
 import HealthKit
+import Charts
 
 struct ContentView: View {
-    @StateObject private var healthKitManager = HealthKitManager()
+    @StateObject private var healthKitManager = HealthKitManager.shared
+    
     var body: some View {
+        VStack {
+            Text("Step Count Chart")
+                .font(.title)
+                .padding()
+            
+            if let stepDataDict = healthKitManager.aggregatedData[.stepCount] {
+                let stepDataArray = stepDataDict.map { (date, steps) in
+                    StepData(date: date, steps: steps)
+                }
+                .sorted { $0.date < $1.date }
+
+                Chart(stepDataArray) { item in
+                    BarMark(
+                        x: .value("Date", item.date),
+                        y: .value("Steps", item.steps)
+                    )
+                    .foregroundStyle(.blue)
+                }
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) { value in
+                        AxisGridLine()
+                        AxisTick()
+                        AxisValueLabel(format: .dateTime.day().month())
+                    }
+                }
+                .frame(height: 300)
+                .padding()
+            } else {
+                Text("No data available")
+            }
+        }
         VStack {
             HStack {
                 Text("Nhịp tim")
-                Text("..........")
+                Text("\(healthKitManager.quantityValues[.heartRate] ?? 0, specifier: "%.1f")")
             }
             HStack {
                 Text("Huyết áp")
-                Text("..........")
+                Text("\(healthKitManager.quantityValues[.bloodPressureSystolic] ?? 0, specifier: "%.0f") / \(healthKitManager.quantityValues[.bloodPressureDiastolic] ?? 0, specifier: "%.0f") mmHg")
             }
             HStack {
                 Text("Bước chạy")
-                Text("..........")
+                Text("\(healthKitManager.quantityValues[.stepCount] ?? 0, specifier: "%.0f")")
             }
+            Button {
+                healthKitManager.requestAuthorization()
+            } label: {
+                Text("Request authorization")
+            }
+            .padding()
+            .background(.yellow)
+
         }
         .padding()
     }
+}
+
+// Struct dữ liệu chart
+struct StepData: Identifiable {
+    var id: String = UUID().uuidString
+    var date: Date
+    var steps: Double
 }
 
 #Preview {
--- HealthAppTestSettingCodeYourselft/HealthKitManager.swift
+++ HealthAppTestSettingCodeYourselft/HealthKitManager.swift
@@ -10,33 +10,45 @@ import HealthKit
 
 
 class HealthKitManager : ObservableObject {
-    @Published var bloodPressure: (systolic: Double, diastolic: Double)?
-    @Published var heartRate: Double?
+//    @Published var bloodPressure: (systolic: Double, diastolic: Double)?
+    @Published var correlationValues: [HKCorrelationTypeIdentifier: [HKCorrelation]] = [:]
+    @Published var quantityValues: [HKQuantityTypeIdentifier: Double] = [:]
+    @Published var categorySamples: [HKCategoryTypeIdentifier: [HKCategorySample]] = [:]
+    @Published var workouts: [HKWorkoutActivityType: [HKWorkout]] = [:]
+    // Gộp steps theo interval
+    @Published var aggregatedData: [HKQuantityTypeIdentifier: [Date: Double]] = [:]
+    
     
     let healthStore = HKHealthStore()
-    init() {
-        enableBackgroundDelivery(types: dataTypes)
-    }
+    static let shared = HealthKitManager()
     private var anchors: [HKSampleType: HKQueryAnchor] = [:]
     
     var shareTypes: Set<HKSampleType> = [
         HKObjectType.quantityType(forIdentifier: .heartRate)!,
         HKObjectType.quantityType(forIdentifier: .bloodPressureSystolic)!,
         HKObjectType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
-        HKObjectType.correlationType(forIdentifier: .bloodPressure)!
+        HKObjectType.quantityType(forIdentifier: .stepCount)!
     ]
     
     var readTypes: Set<HKObjectType> = [
         HKObjectType.quantityType(forIdentifier: .heartRate)!,
-        HKObjectType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
         HKObjectType.quantityType(forIdentifier: .bloodPressureSystolic)!,
-        HKObjectType.correlationType(forIdentifier: .bloodPressure)!
+        HKObjectType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
+        HKObjectType.quantityType(forIdentifier: .stepCount)!
     ]
     
     let dataTypes: [HealthDataTypeIdentifier] = [
         .quantity(.heartRate),
-        .correlation(.bloodPressure)
+        .quantity(.bloodPressureSystolic),
+        .quantity(.bloodPressureDiastolic),
+        .quantity(.stepCount)
     ]
+    
+    private init() {
+        DispatchQueue.main.async{
+            self.enableBackgroundDelivery(types: self.dataTypes)
+        }
+    }
     
     func enableBackgroundDelivery(types: [HealthDataTypeIdentifier]) {
         for type in types {
@@ -52,7 +64,33 @@ class HealthKitManager : ObservableObject {
         }
     }
     
-    func checkAuthorizationStatues(allTypes: [HKObjectType])->Bool {
+    func requestAuthorization() {
+        healthStore.requestAuthorization(toShare: shareTypes, read: readTypes) { success, error in
+            if success {
+                let types: [HKQuantityType] = [HKQuantityType(.heartRate),HKQuantityType(.bloodPressureSystolic), HKQuantityType(.bloodPressureDiastolic)]
+                if(self.checkAuthorizationStatuses(allTypes: types)){
+                    let queueGlobal = DispatchQueue.global(qos: .userInitiated)
+                    queueGlobal.async {
+                        self.startObserver(typeIndentifier: .quantity(.heartRate), isStatisTiced: false)
+                    }
+                    queueGlobal.async {
+                        self.startObserver(typeIndentifier: .quantity(.bloodPressureDiastolic), isStatisTiced: false)
+                    }
+                    queueGlobal.async {
+                        self.startObserver(typeIndentifier: .quantity(.bloodPressureSystolic), isStatisTiced: false)
+                    }
+                    queueGlobal.async {
+                        self.startObserver(typeIndentifier: .quantity(.stepCount), isStatisTiced: true,interval: ChartInterval.day)
+                    }
+
+                }
+            } else {
+                print("errors")
+            }
+        }
+    }
+    
+    func checkAuthorizationStatuses(allTypes: [HKObjectType])->Bool {
         let allAuthorized = allTypes.allSatisfy { type in
             let status = healthStore.authorizationStatus(for: type)
             return status == .sharingAuthorized
@@ -65,7 +103,7 @@ class HealthKitManager : ObservableObject {
         return status == .sharingAuthorized
     }
     
-    func fetchValueQuantity(typeIndentifier: HealthDataTypeIdentifier) {
+    func startObserver(typeIndentifier: HealthDataTypeIdentifier, isStatisTiced: Bool, interval:ChartInterval? = nil) {
         guard let sampleType = typeIndentifier.toSampleType() else {
                 fatalError("*** Unable to get the sample type ***")
             }
@@ -77,11 +115,31 @@ class HealthKitManager : ObservableObject {
             }
             
             self.fetchLatestValueWithAnchor(typeIdentifier: typeIndentifier)
+            if(isStatisTiced){
+                guard let interval = interval else { return }
+                let startDate:Date
+                let calendar = Calendar.current
+                switch interval {
+                case .day:
+                    startDate = calendar.date(byAdding: .day, value: -7, to: Date())!
+                case .week:
+                    startDate = calendar.date(byAdding: .weekOfYear, value: -1, to: Date())!
+                case .month:
+                    startDate = calendar.date(byAdding: .month, value: -1, to: Date())!
+                case .year:
+                    startDate = calendar.date(byAdding: .year, value: -1, to: Date())!
+                }
+                if let samepleType = typeIndentifier.toSampleType() as? HKQuantityType{
+                    self.fetchValueForChart(type: samepleType, interval: interval, startDate: startDate)
+                }
+                
+            }
             print("Có thay đổi mới cho \(sampleType)")
             completion()
         }
         healthStore.execute(query)
     }
+    
     
     func fetchLatestValuesWithAnchor(typeIdentifiers: [HealthDataTypeIdentifier]) {
         for identifier in typeIdentifiers {
@@ -95,11 +153,40 @@ class HealthKitManager : ObservableObject {
         runAnchoredQuery(for: sampleType)
     }
     
+    func fetchValueForChart(type: HKQuantityType, interval: ChartInterval, startDate: Date) {
+        let endDate = Date()
+        let query = HKStatisticsCollectionQuery(
+            quantityType: type,
+            quantitySamplePredicate: nil,
+            options: .cumulativeSum,
+            anchorDate: startDate,
+            intervalComponents: interval.dateComponents
+        )
+        query.initialResultsHandler = { [weak self] _, results, _ in
+            guard let self = self, let results = results else { return }
+            
+            var newData: [Date: Double] = [:]
+            results.enumerateStatistics(from: startDate, to: endDate) { stat, _ in
+                if let sum = stat.sumQuantity() {
+                    newData[stat.startDate] = sum.doubleValue(for: .count())
+                }
+            }
+            
+            DispatchQueue.main.async {
+                let stringId = type.identifier
+                let identifier = HKQuantityTypeIdentifier(rawValue: stringId)
+                self.aggregatedData[identifier] = newData
+            }
+        }
+        
+        healthStore.execute(query)
+    }
+    
     func runAnchoredQuery(for sampleType: HKSampleType) {
         let query = HKAnchoredObjectQuery(
             type: sampleType,
             predicate: nil,
-            anchor: anchors[sampleType], // mỗi sampleType có anchor riêng
+            anchor: anchors[sampleType] ?? nil, // mỗi sampleType có anchor riêng
             limit: HKObjectQueryNoLimit
         ) { [weak self] (_, samplesOrNil, _, newAnchor, errorOrNil) in
             if let error = errorOrNil {
@@ -115,15 +202,48 @@ class HealthKitManager : ObservableObject {
                 for sample in samples {
                     switch sample {
                     case let quantitySample as HKQuantitySample:
-                        let value = quantitySample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: .minute()))
-                        print("Quantity [\(sampleType.identifier)]: \(value) at \(quantitySample.startDate)")
-
+                        let stringId = quantitySample.quantityType.identifier
+                        let identifier = HKQuantityTypeIdentifier(rawValue: stringId)
+                        let unit: HKUnit
+                        switch identifier {
+                        case .heartRate:
+                            unit = HKUnit.count().unitDivided(by: .minute())
+                        case .stepCount:
+                            unit = HKUnit.count()
+                        case .distanceWalkingRunning:
+                            unit = HKUnit.meter()
+                        case .bloodPressureSystolic, .bloodPressureDiastolic:
+                            unit = HKUnit.millimeterOfMercury()
+                        default:
+                            unit = HKUnit.count()
+                        }
+                        let value = quantitySample.quantity.doubleValue(for: unit)
+                        DispatchQueue.main.async{
+                            self?.quantityValues[identifier] = value
+                        }
+                    case let correlationSample as HKCorrelation:
+                        let stringId = correlationSample.correlationType.identifier
+                        let identifier = HKCorrelationTypeIdentifier(rawValue: stringId)
+                        var list = self?.correlationValues[identifier] ?? []
+                        list.append(correlationSample)
+                        DispatchQueue.main.async{
+                            self?.correlationValues[identifier] = list
+                        }
+                        
                     case let categorySample as HKCategorySample:
-                        print("Category [\(sampleType.identifier)]: \(categorySample.value) at \(categorySample.startDate)")
-
+                        let stringId = categorySample.categoryType.identifier
+                        let identifier = HKCategoryTypeIdentifier(rawValue: stringId)
+                        var list = self?.categorySamples[identifier] ?? []
+                        list.append(categorySample)
+                        DispatchQueue.main.async{
+                            self?.categorySamples[identifier] = list
+                        }
+                        
                     case let workout as HKWorkout:
-                        print("Workout: \(workout.workoutActivityType.rawValue) duration: \(workout.duration)")
-
+                        let type = workout.workoutActivityType
+                        var list = self?.workouts[type] ?? []
+                        list.append(workout)
+                        self?.workouts[type] = list
                     default:
                         print("Unhandled sample: \(sample)")
                     }
@@ -133,17 +253,7 @@ class HealthKitManager : ObservableObject {
 
         healthStore.execute(query)
     }
-    func requestAuthorization() {
-        healthStore.requestAuthorization(toShare: shareTypes, read: readTypes) { success, error in
-//            if success {
-//                if(self.checkAuthorizationStatus(type:))
-//                self.fetchValueQuantity(typeIndentifier: .quantity(.heartRate))
-//                
-//            } else {
-//                print("errors")
-//            }
-        }
-    }
+    
 }
 
 
@@ -178,6 +288,22 @@ extension HealthKitManager {
 
             case .workout:
                 return HKObjectType.workoutType()
+            }
+        }
+    }
+    
+    enum ChartInterval {
+        case day
+        case week
+        case month
+        case year
+        
+        var dateComponents: DateComponents {
+            switch self {
+            case .day: return DateComponents(day: 1)
+            case .week: return DateComponents(day: 7)
+            case .month: return DateComponents(month: 1)
+            case .year: return DateComponents(year: 1)
             }
         }
     }
